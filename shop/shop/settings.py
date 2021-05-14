6ca1358325f59a316d6c7adeea438bdacc28a87d
aidar <aidar.asakeev@gmail.com>
2021-05-14 23:57:24 +0600

    Deploying to heroku 4

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -116,6 +116,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'api_root'
 
